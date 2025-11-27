facd712ac720c35b9f0fa0ca989f997577a0d6c3
saif lotfy <saif lotfy@LAPTOP-E4C8H883>
2025-11-27 16:21:29 +0200

    Type View and Controller

---
+++ AdminDashBoardV1.0.0/Controllers/TypeController.cs
@@ -0,0 +1,36 @@
+﻿using Ecommerce.Domain.Models.Contracts.UOW;
+using Ecommerce.Domain.Models.Products;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdminDashBoardV1._0._0.Controllers
+{
+    public class TypeController(IUnitOfWork _unitOfWork) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var types = await _unitOfWork.GetRepository<ProductType, int>().GetAllAsync();
+            return View(types);
+        }
+        public async Task<IActionResult> Create(ProductType type)
+        {
+            try
+            {
+                _unitOfWork.GetRepository<ProductType, int>().add(type);
+                _unitOfWork.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            catch (System.Exception)
+            {
+                ModelState.AddModelError("Name", "Please enter new name");
+                return View("Index", await _unitOfWork.GetRepository<ProductType, int>().GetAllAsync());
+            }
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var type = await _unitOfWork.GetRepository<ProductType, int>().GetByIdAsync(id);
+            _unitOfWork.GetRepository<ProductType, int>().delete(type);
+            await _unitOfWork.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
